653b0a285ef26db3ba079c8b395aab7b0394f557
Vijay9942 <vijayganapathi.r@gmail.com>
2024-11-12 13:15:08 +0530

    create my profile page

---
+++ lib/myprofile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Myprofile extends StatelessWidget {
+  const Myprofile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Profile'),
+     backgroundColor: Colors.white10,
+
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                'Sindhu',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                  fontSize: 30,
+                ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
